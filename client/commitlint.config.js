f7fe9322550a1dae77f8cefd6c2f4ed60e654653
Sant Deleon <sant@santdeleon.io>
2020-12-24 15:01:26 -0600

    chore: move husky config and configure commitlint
    moved husky config back to package.json to allow git hooks | configured commitlint

--- client/commitlint.config.js
+++ client/commitlint.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  extends: "@commitlint/config-conventional",
-  rules: {
-    "type-case": [0, "never"],
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "fix",
-        "docs",
-        "style",
-        "refactor",
-        "perf",
-        "test",
-        "chore",
-        "revert",
-        "wip",
-      ],
-    ],
-  },
+  extends: '@commitlint/config-conventional',
 };
